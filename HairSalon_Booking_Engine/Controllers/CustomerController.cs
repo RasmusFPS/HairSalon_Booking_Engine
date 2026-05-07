0679b35f5b2d0fa63da12475ac23e80ee2fbb29d
Izabella <bellanthie@gmail.com>
2026-05-07 15:29:22 +0200

    created getCustomers endpoint

--- HairSalon_Booking_Engine/Controllers/CustomerController.cs
+++ HairSalon_Booking_Engine/Controllers/CustomerController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HairSalon_Booking_Engine.Models.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace HairSalon_Booking_Engine.Controllers
 {
@@ -12,5 +14,24 @@ namespace HairSalon_Booking_Engine.Controllers
         {
             _ctx = ctx;
         }
+
+        [HttpGet(Name = "GetCustomers")]
+        public async Task<ActionResult<ICollection<GetCustomerResponse>>> GetCustomers()
+        {
+            return Ok(await _ctx.Customers
+                .AsNoTracking()
+                .Select(c => new GetCustomerResponse
+                {
+                    Id = c.Id,
+                    FirstName = c.FirstName,
+                    LastName = c.LastName,
+                    Phone = c.Phone,
+                    Email = c.Email
+
+                }).ToListAsync());
+        }
+
+
+
     }
 }
