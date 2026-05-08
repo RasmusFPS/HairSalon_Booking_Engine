29b4c20f579650aead4ebcb96271184d07af7fef
liamb <liambredahl03@gmail.com>
2026-05-08 22:26:28 +0200

    Added a BookingController

---
+++ HairSalon_Booking_Engine/Controllers/BookingController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HairSalon_Booking_Engine.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookingController : ControllerBase
+    {
+        private readonly HairSalonDBContext _ctx;
+
+        public BookingController(HairSalonDBContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+
+    }
+}
