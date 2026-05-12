17f4a508bfc5d544ec7bf41e6abee34259595221
Izabella <bellanthie@gmail.com>
2026-05-12 12:10:34 +0200

    created GetCustomerById

--- HairSalon_Booking_Engine.Models/DTOs/BookingDTOs.cs
+++ HairSalon_Booking_Engine.Models/DTOs/BookingDTOs.cs
@@ -2,6 +2,9 @@
 
 namespace HairSalon_Booking_Engine.Models.DTOs
 {    
+
+    // kanske lägga till ANNOTATIONS inom DTOS
+    // tänk på Fluent Validations
     public record GetBookingRequest(DateTime BookingDate, DateTime BookedDate, int StylistId, int CustomerId);
     public record CreateBookingRequest(DateTime BookingDate, DateTime BookedDate, int StylistId, int CustomerId);
 
