552ac65661f5c45e1c6f50809cf875a7a3af5faa
Rasmus <Rasmus.andersen05@gmail.com>
2026-05-18 16:33:32 +0200

    Uncommented the services

--- HairSalon_Booking_Engine/Services/IBookingService.cs
+++ HairSalon_Booking_Engine/Services/IBookingService.cs
@@ -1,14 +1,13 @@
 ﻿using HairSalon_Booking_Engine.Models.DTOs;
 
 namespace HairSalon_Booking_Engine.Services
-
 {
     public interface IBookingService
     {
         Task<IEnumerable<GetBookingResponse>> GetAllAsync();
-        Task<GetBookingResponse?> GetByIdAsync(int id);
-        //Task<ServiceResult<GetBookingResponse>> CreateAsync(CreateBookingRequest bookingRequest);
-        //Task<ServiceResult> UpdateAsync(int id, CreateBookingRequest updatedBooking);
-        //Task<ServiceResult> DeleteAsync(int id);
+        Task<GetBookingResponse> GetByIdAsync(int id);
+        Task<ServiceResult<GetBookingResponse>> CreateAsync(CreateBookingRequest bookingRequest);
+        Task<ServiceResult> UpdateAsync(int id, CreateBookingRequest updatedBooking);
+        Task<ServiceResult> DeleteAsync(int id);
     }
 }
