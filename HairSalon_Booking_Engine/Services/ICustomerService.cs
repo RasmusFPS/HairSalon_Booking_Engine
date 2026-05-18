6ab74135e36f465377a9ed860f133b60da297752
Izabella <bellanthie@gmail.com>
2026-05-18 10:21:50 +0200

    created Service Interfaces: customer & booking. Added 2 classes for each of these.

---
+++ HairSalon_Booking_Engine/Services/ICustomerService.cs
@@ -0,0 +1,13 @@
+﻿using HairSalon_Booking_Engine.Models.DTOs;
+
+namespace HairSalon_Booking_Engine.Services
+{
+    public interface ICustomerService
+    {
+        Task<IEnumerable<GetCustomerResponse>> GetAllAsync();
+        Task<GetCustomerResponse?> GetByIdAsync(int id);
+        //Task<ServiceResult<GetCustomerResponse>> CreateAsync(CreateCustomerRequest customerRequest);
+        //Task<ServiceResult> UpdateAsync(int id, CreateCustomerRequest updatedCustomer);
+        //Task<ServiceResult> DeleteAsync(int id);
+    }
+}
