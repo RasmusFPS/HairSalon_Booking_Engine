421f44837a29d219bcdcb9103838e974f51d7572
Albert <Albert_Jyllhed@hotmail.com>
2026-05-21 11:20:10 +0200

    Added fake DbContextFactory method for use in Service tests

---
+++ HairSalon_Booking_Engine.Tests/TestData/DbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HairSalon_Booking_Engine.Tests.TestData
+{
+    internal static class DbContextFactory
+    {
+        /// <summary>
+        /// Metod för att skapa en fake databas som kan användas i Service tester.
+        /// </summary>
+        /// <param name="dbName">Namnet på fake databasen.</param>
+        /// <returns>Returnerar en ny HairSalonDBContext.</returns>
+        public static HairSalonDBContext Create(string dbName)
+        {
+            var options = new DbContextOptionsBuilder<HairSalonDBContext>()
+                .UseInMemoryDatabase(databaseName: dbName)
+                .Options;
+
+            return new HairSalonDBContext(options);
+        }
+    }
+}
