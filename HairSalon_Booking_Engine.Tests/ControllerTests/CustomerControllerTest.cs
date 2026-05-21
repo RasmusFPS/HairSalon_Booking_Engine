1a7cbe77ec6ef501a2328aa69f2498b0001bfb93
Albert <Albert_Jyllhed@hotmail.com>
2026-05-21 23:31:20 +0200

    Added Update Customer Controller tests

--- HairSalon_Booking_Engine.Tests/ControllerTests/CustomerControllerTest.cs
+++ HairSalon_Booking_Engine.Tests/ControllerTests/CustomerControllerTest.cs
@@ -1,11 +1,48 @@
+using FluentValidation;
+using HairSalon_Booking_Engine.Controllers;
+using HairSalon_Booking_Engine.Models.DTOs;
+using HairSalon_Booking_Engine.Services;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
 namespace HairSalon_Booking_Engine.Tests.ControllerTests;
 
 [TestClass]
 public class CustomerControllerTest
 {
-    [TestMethod]
-    public void TestMethod1()
-    {
+    private Mock<ICustomerService> _serviceMock = null!;
+    private Mock<IValidator<CreateCustomerRequest>> _validatorMock = null!;
+    private CustomerController _controller = null!;
 
+    [TestInitialize]
+    public void Setup()
+    {
+        _serviceMock = new Mock<ICustomerService>();
+        _validatorMock = new Mock<IValidator<CreateCustomerRequest>>();
+        _controller = new CustomerController(_serviceMock.Object, _validatorMock.Object);
+    }
+
+    [TestMethod]
+    public async Task Update_ExistingCustomer_ReturnsNoContent()
+    {
+        var request = new CreateCustomerRequest("Anna", "Berg", "+46701234567", null);
+        _serviceMock.Setup(s => s.UpdateAsync(1, request)).ReturnsAsync(ServiceResult.Ok());
+
+        var actionResult = await _controller.Update(1, request);
+
+        Assert.IsInstanceOfType(actionResult, typeof(NoContentResult));
+    }
+
+    [TestMethod]
+    public async Task Update_NonExistingCustomer_ReturnsNotFound()
+    {
+        var request = new CreateCustomerRequest("Anna", "Berg", "+46701234567", null);
+        _serviceMock
+            .Setup(s => s.UpdateAsync(999, request))
+            .ReturnsAsync(ServiceResult.NotFound("Ingen kund hittades med ID: 999"));
+
+        var actionResult = await _controller.Update(999, request);
+
+        Assert.IsInstanceOfType(actionResult, typeof(NotFoundObjectResult));
     }
 }
