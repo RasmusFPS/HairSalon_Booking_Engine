66f7475a7ada9a093bda92cbb49a0922f50c24db
Albert <Albert_Jyllhed@hotmail.com>
2026-05-29 16:20:52 +0200

    Updated GetBookingResponse DTO to include Treatments

--- HairSalon_Booking_Engine.Models/DTOs/BookingDTOs.cs
+++ HairSalon_Booking_Engine.Models/DTOs/BookingDTOs.cs
@@ -1,10 +1,17 @@
 ﻿namespace HairSalon_Booking_Engine.Models.DTOs
 {    
-    public record GetBookingResponse(int Id, DateTime CreatedAt, DateTime StartTime, GetStylistResponse Stylist, GetCustomerResponse Customer);
+    public record GetBookingResponse(
+        int Id, 
+        DateTime CreatedAt, 
+        DateTime StartTime, 
+        GetStylistResponse Stylist, 
+        GetCustomerResponse Customer, 
+        ICollection<GetTreatmentResponse> Treatments);
     public record CreateBookingRequest(DateTime StartTime, int StylistId, int CustomerId);
     public record UpdateBookingRequest(DateTime StartTime, int StylistId, int CustomerId);
 
     // Kanske borde flyttas in till en egen StylistDTOs?
     // Kanske är onödigt om det bara ska finnas en DTO?
     public record GetStylistResponse(int Id, string FirstName, string LastName);
+    public record GetTreatmentResponse(int Id, string Name, string? Description, decimal Price, int DurationMin);
 }
