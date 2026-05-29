7734e6399b719b92de2b5f7698efc8095527a566
Rasmus <Rasmus.andersen05@gmail.com>
2026-05-29 16:09:14 +0200

    Created GetById Stylist Endpoint

--- HairSalon_Booking_Engine/Services/IScheduleService.cs
+++ HairSalon_Booking_Engine/Services/IScheduleService.cs
@@ -5,7 +5,7 @@ namespace HairSalon_Booking_Engine.Services
     public interface IScheduleService
     {
         Task<IEnumerable<GetScheduleResponse>> GetAllAsync();
-        //Task<GetScheduleResponse?> GetByIdAsync(int id);
+        Task<List<GetScheduleResponse>> GetByIdAsync(int id);
         //Task<ServiceResult<GetScheduleResponse>> CreateAsync(CreateScheduleRequest customerRequest);
         //Task<ServiceResult> UpdateAsync(int id, UpdateScheduleRequest updatedCustomer);
         //Task<ServiceResult> DeleteAsync(int id);
