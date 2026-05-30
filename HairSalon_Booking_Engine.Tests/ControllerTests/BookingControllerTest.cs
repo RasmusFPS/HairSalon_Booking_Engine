0d63dd0e0eb2d8615ce72121a908e983a9621f74
Rasmus <Rasmus.andersen05@gmail.com>
2026-05-30 11:24:16 +0200

    Added new parameters that were added with the DTO to fix test

--- HairSalon_Booking_Engine.Tests/ControllerTests/BookingControllerTest.cs
+++ HairSalon_Booking_Engine.Tests/ControllerTests/BookingControllerTest.cs
@@ -1,5 +1,6 @@
 ﻿using FluentValidation;
 using HairSalon_Booking_Engine.Controllers;
+using HairSalon_Booking_Engine.Models;
 using HairSalon_Booking_Engine.Models.DTOs;
 using HairSalon_Booking_Engine.Services;
 using Microsoft.AspNetCore.Mvc;
@@ -22,27 +23,35 @@ namespace HairSalon_Booking_Engine.Tests.ControllerTests
             _controller = new BookingController(_serviceMock.Object, _validatorMock.Object);
         }
 
-        //[TestMethod]
-        //public async Task GetAllBooking_ReturnsOkWithBookings()
-        //{
-        //    //Arrange
-        //    var bookings = new List<GetBookingResponse>
-        //    {
-        //        new(1,
-        //        DateTime.Today,
-        //        DateTime.Today.AddDays(1),
-        //        new GetStylistResponse(1, "Stylist", "Name"),
-        //        new GetCustomerResponse(1, "Customer", "Name", "+46701234567", "customer@email.se")),
-        //    };
-        //    _serviceMock.Setup(s => s.GetAllAsync()).ReturnsAsync(bookings);
+        [TestMethod]
+        public async Task GetAllBooking_ReturnsOkWithBookings()
+        {
+            //Arrange
+            var bookings = new List<GetBookingResponse>
+            {
+                new(1,
+                    DateTime.Today,
+                    DateTime.Today.AddDays(1),
+                    DateTime.Today.AddDays(1).AddHours(1),
+                    BookingStatus.Pending,
+                    new GetStylistResponse(1, "Stylist", "Name"),
+                    new GetCustomerResponse(1, "Customer", "Name", "+46701234567", "customer@email.se"),
+                    new List<GetTreatmentResponse>() 
+                    {
+                        new GetTreatmentResponse(1, "haircut", "haircut", 120, 120)
+                    }
+                )
 
-        //    //Act
-        //    var actionResult = await _controller.GetAll();
+            };
+            _serviceMock.Setup(s => s.GetAllAsync()).ReturnsAsync(bookings);
 
-        //    //Assert
-        //    var ok = actionResult.Result as OkObjectResult;
-        //    Assert.IsNotNull(ok);
-        //    Assert.AreEqual(bookings, ok.Value);
-        //}
+            //Act
+            var actionResult = await _controller.GetAll();
+
+            //Assert
+            var ok = actionResult.Result as OkObjectResult;
+            Assert.IsNotNull(ok);
+            Assert.AreEqual(bookings, ok.Value);
+        }
     }
 }
