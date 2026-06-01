473bbd9ce09714d3e542528062543d3231cb7a0c
Rasmus <Rasmus.andersen05@gmail.com>
2026-06-02 00:36:42 +0200

    Added IAvailabilityService & AvailabilityService

---
+++ HairSalon_Booking_Engine/Services/AvailabilityService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HairSalon_Booking_Engine.Services
+{
+    public class AvailabilityService
+    {
+        private readonly HairSalonDBContext _ctx;
+        public AvailabilityService(HairSalonDBContext ctx)
+        {
+            _ctx = ctx;
+        }
+        
+    }
+}
